4ba1986a3b3d5d38cf4600806a07982b89875e7c
Marcus <mojave27@gmail.com>
2019-02-09 19:52:57 -0700

    updated with working, multi-dropdown items.

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,108 +1,75 @@
-import React, { Component } from 'react';
-import './TopNav.css';
+import React, { Component } from "react";
+import { Link } from "@reach/router";
+import { menuConfig } from './topNavMenuConfig';
 
-const menuConfig = {
-    menuItems: [
-        {   menuName: 'home' },
-        {   menuName: 'stuff' },
-        {   menuName: 'link',
-            a: (<a href='/'>{'link'}</a>),
-        },
-        {
-            menuName: 'calc',
-            dropDownItems: [
-                { text: 'Link 1' },
-                { text: 'Link 2' },
-                { a: <a href='/'>Link 3</a> },
-            ]
-        }
-    ]
+import "./TopNav.css";
+
+class TopNav extends Component {
+  state = {
+    display: {}
+  };
+
+  handleClick = event => {
+    this.toggleDisplay(event, "none");
+  };
+
+  mouseOver = event => {
+    this.toggleDisplay(event, "block");
+  };
+
+  mouseOut = event => {
+    this.toggleDisplay(event, "none");
+  };
+
+  toggleDisplay = (event, displayType) => {
+    const menuName = event.target.getAttribute("menu-name");
+    let display = this.state.display;
+    display[menuName] = displayType;
+    this.setState({ display: display });
+  };
+
+  renderDropDownMenu = (menuConfig) => {
+    let menuName = menuConfig.name;
+    return (
+      <div className="dropdown">
+        <button
+          onMouseOver={this.mouseOver}
+          onMouseOut={this.mouseOut}
+          menu-name={menuName}
+          className="dropbtn"
+        >
+          {menuName}
+          <i className="fa fa-caret-down" />
+        </button>
+        <div
+          style={{ display: this.state.display[menuName] }}
+          menu-name={menuName}
+          onClick={this.handleClick}
+          onMouseOver={this.mouseOver}
+          onMouseOut={this.mouseOut}
+          className="dropdown-content"
+        >
+        {menuConfig.items.map( (menuItem, index) => {
+          return (<Link key={index} menu-name={menuName} to={menuItem.to}>{menuItem.text}</Link>)
+        })}
+        </div>
+      </div>
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <div className="navbar">
+          <Link to="/">home</Link>
+          {this.renderDropDownMenu(menuConfig[0])}
+          {this.renderDropDownMenu(menuConfig[1])}
+          {this.renderDropDownMenu(menuConfig[2])}
+
+        </div>
+      </div>
+    );
+  }
 }
 
-class TopNavWithExternalConfig extends Component {
-    state = {
-        hover: false,
-        display: 'none'
-    }
-
-    mouseOver = () => {
-        this.setState({ hover: true, display: 'block' });
-    }
-
-    mouseOut = () => {
-        this.setState({ hover: false, display: 'none' });
-    }
-
-    menuClick = () => {
-        this.setState(prevState => {
-            let display = 'none';
-            if (prevState.display === 'none') display = 'block';
-            return { hover: false, display: display }
-        });
-    }
-
-    renderMenuContainer = (menuItems) => {
-        let menuContainer = (
-            <div
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                onClick={this.menuClick}
-                className='dropdownContent'
-                style={{ display: this.state.display }} >
-                {menuItems.map( (menuItem, index) => {
-                    return (
-                        <div
-                            key={index}
-                            className='menuItem'
-                            onClick={this.menuClick}
-                        >{menuItem.a ? menuItem.a : menuItem.text}</div>
-                    )
-                })}
-            </div>);
-        return menuContainer;
-    }
-
-    renderDropDownMenu = (menuItem, index) => {
-        let dropDownMenu = (
-            <li className='dropbtn'
-                onMouseOver={this.mouseOver}
-                onMouseOut={this.mouseOut}
-                onClick={this.menuClick}
-                key={index}
-            >
-                <span className='dropbtnLabel'>{menuItem.menuName}</span>
-                {this.renderMenuContainer(menuItem.dropDownItems)}
-            </li>
-        );
-        return dropDownMenu;
-    }
-
-    renderStaticMenuItem = (menuItem, index) => {
-        let innerContent = menuItem.link ? menuItem.link : menuItem.menuName;
-        let staticMenuItem = (
-            <li key={index} className='menubtn'>{innerContent}</li>
-        );
-        return staticMenuItem;
-    }
-
-    renderTopNav = () => {
-        return menuConfig.menuItems.map( (menuItem, index) => {
-            if(menuItem.dropDownItems){
-                return this.renderDropDownMenu(menuItem, index);
-            }else{
-                return this.renderStaticMenuItem(menuItem, index);
-            }
-        })
-    }
-
-    render() {
-        return (
-            <ul id='topnav'>
-                {this.renderTopNav()}
-            </ul>
-        )
-    }
-
-}
-
-export default TopNavWithExternalConfig;
+export default TopNav;
--- src/components/topNavMenuConfig.js
+++ src/components/topNavMenuConfig.js
@@ -1,27 +1,24 @@
-import React from 'react';
-import { Link } from "@reach/router";
 
-export const topNavMenuConfig = () => {
-    return ({
-        menuItems: [
-            { menuName: 'home' },
-            { menuName: 'link',
-                link: (<Link to='/'>{'link'}</Link>),
-            },
-            { menuName: 'stuff' },
-            {
-                menuName: 'link',
-                link: (<a href='/'>{'a-tag'}</a>),
-            },
-            {
-                menuName: 'calc',
-                dropDownItems: [
-                    { text: 'Link 1' },
-                    { text: 'Link 2' },
-                    { a: <a href='/'>Link 3</a> },
-                    // { link: <Link to='/' />}
-                ]
-            }
-        ]
-    })
-}
+export const menuConfig = 
+    [
+        { name: 'calcs',
+            items: [
+                { to: 'form', text: 'form' },
+                { to: 'hovertest', text: 'hovertest' },
+                { to: '/', text: 'home' }
+            ]
+         },
+        { name: 'other',
+            items: [
+                { to: 'form', text: 'form' },
+                { to: 'hovertest', text: 'hover test' }
+            ]
+         },
+        { name: 'test-menu',
+            items: [
+                { to: 'form', text: 'form' },
+                { to: 'hovertest', text: 'hovertest' },
+                { to: '/', text: 'home' }
+            ]
+         },
+    ];
