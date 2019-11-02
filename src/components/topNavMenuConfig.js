39b3f01bde3460178f9d6bb49f5bb62eafb31f6d
mojave27 <mojave27@users.noreply.github.com>
2019-11-02 12:50:08 -0700

    added a non-dropdown in the example config.

--- src/components/topNavMenuConfig.js
+++ src/components/topNavMenuConfig.js
@@ -1,24 +1,35 @@
 
-export const menuConfig = 
+export const menuConfig =
     [
-        { name: 'calcs',
+        {
+            name: 'home',
+            type: 'button',
+            link: { to: '/', text: 'home' }
+        },
+        {
+            name: 'calcs',
+            type: 'dropdown',
             items: [
                 { to: 'form', text: 'form' },
                 { to: 'hovertest', text: 'hovertest' },
                 { to: '/', text: 'home' }
             ]
-         },
-        { name: 'other',
+        },
+        {
+            name: 'other',
+            type: 'dropdown',
             items: [
                 { to: 'form', text: 'form' },
                 { to: 'hovertest', text: 'hover test' }
             ]
-         },
-        { name: 'test-menu',
+        },
+        {
+            name: 'test-menu',
+            type: 'dropdown',
             items: [
                 { to: 'form', text: 'form' },
                 { to: 'hovertest', text: 'hovertest' },
                 { to: '/', text: 'home' }
             ]
-         },
+        },
     ];
